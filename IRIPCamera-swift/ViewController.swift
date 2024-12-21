e06ee24061f131da30f77b632845a0847c7af3de
Phil <irons163@gmail.com>
2024-12-21 10:46:40 +0800

    Init commit

---
+++ IRIPCamera-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IRIPCamera-swift
+//
+//  Created by irons on 2024/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IRIPCamera-swiftTests/IRIPCamera_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  IRIPCamera_swiftTests.swift
+//  IRIPCamera-swiftTests
+//
+//  Created by irons on 2024/12/21.
+//
+
+import Testing
+@testable import IRIPCamera_swift
+
+struct IRIPCamera_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
